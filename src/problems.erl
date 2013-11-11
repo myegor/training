e59ae018b67fce5138033127bef2107705f6e0ea
0xceed <0xceed@mail.ru>
2013-11-11 14:25:51 +0400

    small optimization for rle encode/decode

--- src/problems.erl
+++ src/problems.erl
@@ -20,7 +20,7 @@ d_helper([],_,Acc)     -> Acc.
 
 delete(L,K) -> reverse(d_helper(L,K,[])).
 
-
+s_helper(L,0,_)       -> {[],L};
 s_helper([H|T],1,Acc) -> {reverse([H|Acc]),T}; 
 s_helper([H|T],K,Acc) -> s_helper(T,K-1,[H|Acc]).
 
@@ -42,24 +42,15 @@ s_flatten([H|T],Acc) -> s_flatten(T,[H|Acc]).
 flatten(L) -> reverse(s_flatten(L,[])). 
 
 
-count([H|T],H,Acc) -> count(T,H,1+Acc);
-count([],_,Acc)    -> Acc;
-count([_|_],_,Acc) -> Acc.
-
-rle_encode([],Acc) -> Acc;
-rle_encode([H|T],Acc) -> 
-						 case count(T,H,0) of
-						 	0   -> rle_encode(T,[{H,1}|Acc]);
-						 	Num -> rle_encode(element(2,split(T,Num)),[{H,Num+1}|Acc])
-						 end.
+rle_encode([],Acc)            -> Acc;
+rle_encode([H|T],[{H,C1}|T1]) -> rle_encode(T,[{H,C1+1}|T1]);
+rle_encode([H|T],Acc)         -> rle_encode(T,[{H,1}|Acc]).
 
 encode(L) -> reverse(rle_encode(L,[])).
 
-add_num(_,0,Acc) -> Acc;
-add_num(S,K,Acc) -> add_num(S,K-1,[S|Acc]).
-
 rle_decode([],Acc)        -> Acc;
-rle_decode([{S,C}|T],Acc) -> rle_decode(T,add_num(S,C,Acc)).
+rle_decode([{S,1}|T],Acc) -> rle_decode(T,[S|Acc]);
+rle_decode([{S,C}|T],Acc) -> rle_decode([{S,C-1}|T],[S|Acc]).
 
 decode(L) -> reverse(rle_decode(L,[])). 
 
